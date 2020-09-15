9a1d495eeefafc9716eabf2b0a2ca8cb9d50210f
Jimmy Aldape <jimmy@jimmyaldape.com>
2020-09-14 22:44:59 -0700

    uses replace-in-fle module to replace template

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 
 const chalk = require("chalk");
 const boxen = require("boxen");
-
 const fs = require("fs");
 const { spawn } = require('child_process');
-
+const replace = require('replace-in-file');
 const boxenOptions = {
     padding: 1,
     margin: 1,
@@ -23,14 +22,6 @@ fs.rmdirSync('.git', { recursive: true }, (error) => {
         console.log(chalk.blue.bold(`.git folder deleted.`));
     }
 });
-// copy readme
-fs.copyFileSync('_templates/README.md','README.md', fs.constants.COPYFILE_FICLONE, (error) => {
-    if(error){
-        console.error(chalk.red.bold("Error copying README"));
-        return;
-    }
-    console.log(chalk.blue.bold("Copied README.md from templates"));
-});
 // copy env
 fs.copyFileSync('_templates/.env.example','.env', fs.constants.COPYFILE_FICLONE, (error) =>{
     if(error){
@@ -38,6 +29,14 @@ fs.copyFileSync('_templates/.env.example','.env', fs.constants.COPYFILE_FICLONE,
         return;
     }
     console.log(chalk.blue.bold("Copied env file from templates"));
+});
+// copy readme
+fs.copyFileSync('_templates/README.md','README.md', fs.constants.COPYFILE_FICLONE, (error) => {
+    if(error){
+        console.error(chalk.red.bold("Error copying README"));
+        return;
+    }
+    console.log(chalk.blue.bold("Copied README.md from templates"));
 });
 // scaffold new laravel application
 const laravel = spawn("laravel", ["new", "src"]);
@@ -58,6 +57,19 @@ laravel.on("close", code => {
             console.log(chalk.blue.bold(`_templates folder deleted.`));
         }
     });
+
+    const options = {
+        files: 'package.json',
+        from: '<PROJECT_NAME>',
+        to: process.env.PROJECT_NAME
+    }
+
+    try {
+        let changedFile = replace.sync(options);
+        console.log(chalk.blue.bold(`package.json updated.`));
+    } catch (error) {
+        console.error(chalk.red.bold("Error updating package.json."));
+    }
 
     const git = spawn("git", ["init"]);
     git.on("close", code => {
