544d06747d07e0cfa9f70add287c963872a8db79
Jimmy <jimmy.aldape@medsch.ucr.edu>
2021-02-23 17:16:40 -0800

    swaps fs for fs-extra and reworks logic

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
 const fsx = require("fs-extra");
 const { spawn } = require('child_process');
 const replace = require('replace-in-file');
 const chalk = require("chalk");
 const boxen = require("boxen");
-const boxenOptions = {
-    padding: 1,
-    margin: 1,
-    borderStyle: "round",
-    borderColor: "blue",
-    backgroundColor: "#555555"
-};
 const prompts = require('prompts');
 const questions = [
     {
@@ -28,224 +20,265 @@ const questions = [
     {
         type: 'text',
         name: 'project_name',
-        message: 'PROJECT_NAME'
+        message: 'ENV: PROJECT_NAME'
+    },
+    {
+        type: 'text',
+        name: 'project_description',
+        message: 'ENV: PROJECT_DESCRIPTION',
+        initial: 'This is a description'
     },
     {
         type: 'number',
         name: 'http_port',
-        message: 'HTTP_PORT',
+        message: 'ENV: HTTP_PORT',
         initial: 8000
     },
     {
         type: 'number',
         name: 'mysql_port',
-        message: 'MYSQL_PORT',
+        message: 'ENV: MYSQL_PORT',
         initial: 3306
     },
     {
         type: 'text',
         name: 'mysql_database',
-        message: 'MYSQL_DATABASE',
+        message: 'ENV: MYSQL_DATABASE',
         initial: 'laraveldb'
     },
     {
         type: 'text',
-        name: 'mysql_user',
-        message: 'MYSQL_USER',
+        name: 'mysql_username',
+        message: 'ENV: MYSQL_USERNAME',
         initial: 'dbuser'
     },
     {
         type: 'text',
         name: 'mysql_password',
-        message: 'MYSQL_PASSWORD',
+        message: 'ENV: MYSQL_PASSWORD',
         initial: 'password'
     },
     {
         type: 'text',
         name: 'github_username',
-        message: 'What is your github username?'
+        message: 'What is your github username?',
+        initial: 'acme'
+    },
+    {
+        type: 'text',
+        name: 'author_name',
+        message: "What is the author's name?",
+        initial: 'Mr. Robot'
+    },
+    {
+        type: 'text',
+        name: 'author_email',
+        message: "What is the author's email?",
+        initial: 'robot@acme.com'
+    },
+    {
+        type: 'text',
+        name: 'author_website',
+        message: "What is the author's website?",
+        initial: 'www.somewebsite.dev'
     }
 ];
-let project_name, github_username = '';
+let project_type,
+    project_name,
+    project_description,
+    http_port,
+    mysql_port,
+    mysql_database,
+    mysql_username,
+    mysql_password,
+    github_username,
+    author_name,
+    author_email,
+    author_website = '';
 
-console.log(boxen( chalk.white.bold("Setting up your development environment ..."), boxenOptions ));
+console.log(boxen( chalk.white.bold("Setting up your development environment ..."), {
+    padding: 2,
+    margin: 3,
+    borderStyle: "round",
+    borderColor: "blue",
+} ));
 
-(async ()=> {
-    const response = await prompts(questions);
-    project_name = response['project_name'].replace(/\s/g,'').toLowerCase();
-    github_username = response['github_username'];
+async function init() {
+        const answer = await prompts(questions);
+        project_type = answer.project_type;
+        project_name = answer.project_name.replace(/\s/g,'').toLowerCase();
+        project_description = answer.project_description;
+        github_username = answer.github_username;
+        author_name = answer.author_name;
+        author_email = answer.author_email;
+        author_website = answer.author_website;
+        http_port = answer.http_port;
+        mysql_port = answer.mysql_port;
+        mysql_database = answer.mysql_database;
+        mysql_username = answer.mysql_username;
+        mysql_password = answer.mysql_password;
+}
 
-    createProjectDirectory(response);
 
+function scaffoldNewApplication(){
+    console.log(chalk.blue.bold(`Scaffolding ${project_type} project ...`));
     let command = '';
     let parameters = [];
-    switch (response['project_type']){
+    switch (project_type){
         case 'laravel':
             command = 'laravel';
-            parameters.push(['new', `../${project_name}/src`]);
+            parameters.push(['new', `${project_name}`]);
             break;
         case 'statamic':
             command = 'composer';
-            parameters.push(['create-project', '--prefer-dist', 'statamic/statamic', `../${project_name}/src`]);
+            parameters.push(['create-project', '--prefer-dist', 'statamic/statamic', `${project_name}`]);
             break;
     }
 
     // scaffold new application
     const project = spawn(command, ...parameters);
     // once finished copy and clean files
-    project.on("close", result => {
-        copyGitIgnore();
-        updateLaravelEnvFile(response);
-        const git = spawn("git", ["init", `../${project_name}`]);
-        git.on("close", code => {
-            console.log(boxen(chalk.white.bold(`Development environment complete. Please 'cd  ../${project_name}' to get started.`), boxenOptions));
-        });
+    project.on("exit", result => {
+        configureEnvironment();
     });
-})();
-
-function createProjectDirectory(response){
+}
+function configureEnvironment(){
+    console.log(chalk.blue.bold("Creating a new git directory ..."));
     let directory = `../${project_name}`;
-    if(!fs.existsSync(directory)){
-        fs.mkdirSync(directory);
-    }
-
-    writeEnvToRoot(response);
-    copyTemplateFiles();
-    copyTemplateFolders();
+    // create directory if needed
+    fsx.ensureDir(directory)
+        .then(()=>{ moveNewApplication(); })
+        .catch( err => { console.log(err); });
 }
+function moveNewApplication(){
+    console.log(chalk.blue.bold(`Moving the ${project_name} project into the new git directory ...`));
+    fsx.move(`${project_name}`, `../${project_name}/src`)
+        .then( () => { copyTemplateFiles(); })
+        .catch( err => { console.log(err); });
+}
+function copyTemplateFiles() {
+    console.log(chalk.blue.bold("Copying template files ..."));
 
-function writeEnvToRoot(response){
-    let file = `../${project_name}/.env`;
-    // override env
-    fs.open(file, 'w', function (err) {
-        if (err) throw err;
-    });
+    const directory = '_templates/';
 
-    const keys = Object.keys(response);
+    fsx.readdir(directory, (err, files) => {
+        if(err) throw err;
 
-    keys.forEach((key, index) => {
-        fs.appendFile(file, `${key.toUpperCase()}=${response[key]} \n`,(error)=>{
-            if(error){
-                throw error;
-            }
+        files.forEach(file => {
+            fsx.copy(`${directory}/${file}`, `../${project_name}/${file.replace('_','')}`, err => {
+                if (err) throw err;
+            });
         });
-    });
 
-    fs.copyFileSync('_templates/_.env.example',`../${project_name}/.env.example`, fs.constants.COPYFILE_FICLONE, (error) => {
-        if(error){
-            throw error;
-        }
-        console.log(chalk.blue.bold("Copied example env from templates"));
+        fsx.copy(`${directory}/_.env.example`,`../${project_name}/.env`)
+            .then(() => { copyMiscFiles(); })
+            .catch( err => { console.log(err); });
     });
 }
+function copyMiscFiles(){
+    console.log(chalk.blue.bold("Updating misc files ..."));
+    fsx.copy(`../${project_name}/.gitignore`, `../${project_name}/src/.gitignore`)
+        .then(() => {  })
+        .catch( err => { console.log(err); });
 
-function copyGitIgnore(){
-    fs.copyFileSync('_templates/_.gitignore', `../${project_name}/src/.gitignore`, fs.constants.COPYFILE_FICLONE, (error) => {
-        if (error) {
-            throw error;
-        }
-        console.log(chalk.blue.bold("Copied .gitignore from templates"));
-    });
-    fs.copyFileSync('_templates/_.gitignore', `../${project_name}/.gitignore`, fs.constants.COPYFILE_FICLONE, (error) => {
-        if (error) {
-            throw error;
-        }
-        console.log(chalk.blue.bold("Copied .gitignore from templates"));
-    });
+    fsx.move(`../${project_name}/.php_cs`, `../${project_name}/src/.php_cs`)
+        .then( () => { updateRootEnv(); })
+        .catch( err => { console.log(err); });
 }
-
-function copyTemplateFiles(){
-    fs.copyFileSync('_templates/_README.md',`../${project_name}/README.md`, fs.constants.COPYFILE_FICLONE, (error) => {
-        if(error){
-            throw error;
-        }
-        console.log(chalk.blue.bold("Copied README.md from templates"));
-    });
-    fs.copyFileSync('_templates/_package.json',`../${project_name}/package.json`, fs.constants.COPYFILE_FICLONE, (error) => {
-        if(error){
-            throw error;
-        }
-        console.log(chalk.blue.bold("Copied package.json from templates"));
-    });
-    fs.copyFileSync('_templates/_.npmrc',`../${project_name}/.npmrc`, fs.constants.COPYFILE_FICLONE, (error) => {
-        if(error){
-            throw error;
-        }
-        console.log(chalk.blue.bold("Copied .npmrc from templates"));
-    });
-    fs.copyFileSync('_templates/_LICENSE',`../${project_name}/LICENSE`, fs.constants.COPYFILE_FICLONE, (error) => {
-        if(error){
-            throw error;
-        }
-        console.log(chalk.blue.bold("Copied LICENSE from templates"));
-    });
-    fs.copyFileSync('_templates/_Dockerfile',`../${project_name}/Dockerfile`, fs.constants.COPYFILE_FICLONE, (error) => {
-        if(error){
-            throw error;
-        }
-        console.log(chalk.blue.bold("Copied Dockerfile from templates"));
-    });
-    fs.copyFileSync('_templates/_docker-compose.yml',`../${project_name}/docker-compose.yml`, fs.constants.COPYFILE_FICLONE, (error) => {
-        if(error){
-            throw error;
-        }
-        console.log(chalk.blue.bold("Copied docker-compose.yml from templates"));
-    });
-
-    updatePlaceholders(
-        `../${project_name}/package.json`,
+function updateRootEnv(){
+    console.log(chalk.blue.bold("Updating root project environment files ..."));
+    let projectEnv = updatePlaceholders(
+        [
+            /{PROJECT_NAME}/g,
+            /{PROJECT_DESCRIPTION}/g,
+            /{GITHUB_USERNAME}/g,
+            /{AUTHOR_NAME}/g,
+            /{AUTHOR_EMAIL}/g,
+            /{AUTHOR_WEBSITE}/g,
+            /{HTTP_PORT}/g,
+            /{MYSQL_PORT}/g,
+            /{MYSQL_PASSWORD}/g,
+            /{MYSQL_USERNAME}/g,
+            /{MYSQL_DATABASE}/g,
+        ],
+        [
+            project_name,
+            project_description,
+            github_username,
+            author_name,
+            author_email,
+            author_website,
+            http_port,
+            mysql_port,
+            mysql_password,
+            mysql_username,
+            mysql_database
+        ],
         `../${project_name}/README.md`,
-        );
+        `../${project_name}/LICENSE`,
+        `../${project_name}/.env`,
+        `../${project_name}/package.json`
+    );
+    projectEnv.then(() => { updateLaravelEnv(); });
+    // projectEnv.then(() => { gitInit(); });
 }
 
-function copyTemplateFolders(){
-    fsx.copy('_templates/_nginx', `../${project_name}/nginx`, (err) => {
-        if(err){
-            throw err;
-        }
-    })
+function updateLaravelEnv(){
+    console.log(chalk.blue.bold(`Updating ${project_type} environment files ...`));
+    let laravelEnv = updatePlaceholders(
+        [
+            /Laravel/g,
+            `APP_URL=http://${project_name}.test`,
+            /DB_HOST=127.0.0.1/g,
+            `DB_DATABASE=${project_name}`,
+            /root/g,
+            /DB_PASSWORD=/g,
+        ],
+        [
+            project_name,
+            `APP_URL=http://localhost:${http_port}`,
+            'DB_HOST=mysql',
+            `DB_DATABASE=${mysql_database}`,
+            mysql_username,
+            `DB_PASSWORD=${mysql_password}`,
+        ],
+        `../${project_name}/src/.env`,
+
+    );
+    laravelEnv.then(()=>{ gitInit(); });
 }
 
-function updatePlaceholders(...files){
+function gitInit(){
+    console.log(chalk.blue.bold("Initializing git directory ..."));
+    const git = spawn("git", ["init", `../${project_name}`]);
+    git.on("exit", code => {
+        console.log(boxen(chalk.white.bold(`Project is ready for development. Please type 'cd  ../${project_name}' to get started.`), {
+            padding: 2,
+            margin: 3,
+            borderStyle: "round",
+            borderColor: "blue",
+        }));
+    });
 
-    try {
-        let changedFile = replace.sync({
-            files: files,
-            from: [/<PROJECT_NAME>/g, /<GITHUB_USERNAME>/g],
-            to: [`${project_name}`, `${github_username}`]
+}
+async function updatePlaceholders(words, replacements,...files ){
+
+    try{
+        words.forEach((word, i) => {
+            let changedFiles = replace.sync({
+                files: files,
+                from: word,
+                to: replacements[i]
+            });
+            // console.log(`Modified files - ${i}:`, changedFiles.join(', '));
         });
+
     } catch (error) {
-        console.error(chalk.red.bold(`Error updating project name placeholders`));
+        console.error('Error occurred: ', error);
     }
 
-    console.log(chalk.blue.bold(`package.json updated.`));
 }
 
-function updateLaravelEnvFile(response){
-
-    try {
-        let changedFile = replace.sync({
-            files: `../${project_name}/src/.env`,
-            from: [
-                'APP_NAME=Laravel',
-                'APP_URL=http://localhost',
-                'DB_HOST=127.0.0.1',
-                'DB_DATABASE=laravel',
-                'DB_USERNAME=root',
-                'DB_PASSWORD='
-            ],
-            to: [
-                `APP_NAME=${project_name}`,
-                `APP_URL=http://localhost:${response['http_port']}`,
-                `DB_HOST=mysql`,
-                `DB_DATABASE=${response['mysql_database']}`,
-                `DB_USERNAME=${response['mysql_user']}`,
-                `DB_PASSWORD=${response['mysql_password']}`
-            ]
-        });
-    } catch (error) {
-        console.error(chalk.red.bold(`Error updating laravel env`));
-    }
-
-    console.log(chalk.blue.bold(`laravel env updated.`));
-}
+init().then(()=> {
+    scaffoldNewApplication()
+})
